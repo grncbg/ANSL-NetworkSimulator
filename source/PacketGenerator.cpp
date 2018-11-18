9084b578b94587f7e3c91fe7951c09b1a14c8903
grncbg <grncbg@gmail.com>
2018-11-19 02:58:27 +0900

    add

--- source/PacketGenerator.cpp
+++ source/PacketGenerator.cpp
@@ -4,3 +4,17 @@
 
 #include "Data.hpp"
 #include "Random.hpp"
+
+template <class T>
+PacketGenerator<T>::PacketGenerator (int arg) {
+
+    this->random = new T(arg);
+
+}
+
+template <class T>
+PacketGenerator<T>::~PacketGenerator () {
+
+    delete(this->random);
+
+}
