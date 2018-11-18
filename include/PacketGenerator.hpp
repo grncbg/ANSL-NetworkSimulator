dc3a14fba9ff772f03c776faf4b837ccfc922d67
grncbg <grncbg@gmail.com>
2018-11-19 02:40:58 +0900

    Update class PacketGenerator
    add function get

--- include/PacketGenerator.hpp
+++ include/PacketGenerator.hpp
@@ -1,9 +1,30 @@
 #ifndef INCLUDED_PACKET_GENERATOR_HPP
 #define INCLUDED_PACKET_GENERATOR_HPP
 
+// dorward declaration
+namespace std {
+
+    // vector
+    template <class T>
+    class allocator;
+    template <class T, class Allocator = allocator<T>>
+    class vector;
+
+}
+// Data
+class Data;
+// Random
+template <class T>
+class Random;
+
+
+//
+// class PacketGenerator
+//
 class PacketGenerator {
 
-
+    template <class T>
+    static std::vector<Data>&& get(int, Random<T>);
 
 };
 
