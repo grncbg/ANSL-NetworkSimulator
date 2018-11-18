2e96bb6086091ae030457583e2be4f98395081c1
grncbg <grncbg@gmail.com>
2018-11-19 00:56:19 +0900

    Update class Random
    Fix constructor, destructor

--- source/Random.cpp
+++ source/Random.cpp
@@ -2,19 +2,15 @@
 
 #include <random>
 
-Random::Random(const int poisson_arg = 0, const double exp_arg = 0.0) {
+Random::Random() {
 
     std::random_device seed_gen;
     this->engine = new std::mt19937_64(seed_gen());
-    this->poisson = new std::poisson_distribution<int>(poisson_arg);
-    this->exponential = new std::exponential_distribution<double>(exp_arg);
 
 }
 
 Random::~Random() {
 
     delete(this->engine);
-    delete(this->poisson);
-    delete(this->exponential);
 
 }
