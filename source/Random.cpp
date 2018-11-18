1816098d71391fa2b87ce988f1cf12a45470bd7d
grncbg <grncbg@gmail.com>
2018-11-19 00:33:52 +0900

    Update class Random
    Add constructor

--- source/Random.cpp
+++ source/Random.cpp
@@ -1,3 +1,12 @@
 #include "Random.hpp"
 
 #include <random>
+
+Random::Random(int poisson_arg = 0, double exp_arg = 0.0) {
+
+    std::random_device seed_gen;
+    this->engine = new std::mt19937_64(seed_gen());
+    this->poisson = new std::poisson_distribution<int>(poisson_arg);
+    this->exponential = new std::exponential_distribution<double>(exp_arg);
+
+}
