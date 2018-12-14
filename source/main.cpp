ecc391f809783c9385c82738773ba351073a3979
grncbg <grncbg@gmail.com>
2018-12-14 16:35:52 +0900

    Add include files

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,12 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <set>
+#include <string>
+
+#include "Data.hpp"
+#include "PacketGenerator.hpp"
+#include "Random.hpp"
 
 using namespace std;
 
