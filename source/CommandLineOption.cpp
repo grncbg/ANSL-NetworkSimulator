a068942ab958869cf347e601685ca8e28adcf676
grncbg <grncbg@gmail.com>
2018-12-16 02:11:06 +0900

    Add constructor

--- source/CommandLineOption.cpp
+++ source/CommandLineOption.cpp
@@ -2,3 +2,10 @@
 
 #include <vector>
 #include <memory>
+
+template <class T>
+CommandLineOption<T>::CommandLineOption(const int argc, const char** argv) {
+    this->argc = argc;
+    this->argv = argv;
+    this->options = new unique_ptr<vector<T>>();
+}
