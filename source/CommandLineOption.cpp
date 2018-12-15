af7bc69c56c27d37b3f134149a3d7215155fc2c5
grncbg <grncbg@gmail.com>
2018-12-16 02:11:19 +0900

    Add parse

--- source/CommandLineOption.cpp
+++ source/CommandLineOption.cpp
@@ -9,3 +9,15 @@ CommandLineOption<T>::CommandLineOption(const int argc, const char** argv) {
     this->argv = argv;
     this->options = new unique_ptr<vector<T>>();
 }
+
+template <>
+bool CommandLineOption<int>::parse() const {
+    if(argc <= 1) {
+        return 0;
+    }
+    options->resize(argc - 1);
+    for(unsigned int i = 0; i < options->size(); i++){
+        (*options)[i] = atoi(argv[i + 1]);
+    }
+    return 0;
+}
