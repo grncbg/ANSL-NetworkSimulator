7d4cac9410d8f995f2f4dc5ce0add14feb3bb08e
grncbg <grncbg@gmail.com>
2018-12-18 19:32:08 +0900

    Add class ReadFile

--- include/File.hpp
+++ include/File.hpp
@@ -1,5 +1,13 @@
 #ifndef INCLUDED_FILE_HPP
 #define INCLUDED_FILE_HPP
 
+class ReadFile {
+private:
+    std::ofstream ofs;
+    const std::filesystem::path path;
+public:
+    ReadFile(std::filesystem::path path): path(path) {}
+    ~ReadFile() = default;
+};
 
 #endif
